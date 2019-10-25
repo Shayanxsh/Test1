6934522d2647f9c0e9ca831e8f10ee2f0c6364d6
Shayan <shayan.shaygani@gmail.com>
2019-10-25 15:31:34 +0330

    Creating test.php file
    test.php is using cURL for extracting data from another web page
    so we just pass options and values to cURL library

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+function get_web_page($url)
+{
+    $user_agent = 'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 FireFox/8.0';
+    $options = array(
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_POST => false,
+        CURLOPT_USERAGENT => $user_agent,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HEADER => false,
+        CURLOPT_FOLLOWLOCATION => true
+    );
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt_array($ch, $options);
+    $content = curl_exec($ch);
+    $err = curl_errno($ch);
+    $errmsg = curl_error($ch);
+    $header = curl_getinfo($ch);
+    curl_close($ch);
+    $header['errorno'] = $err;
+    $header['errmsg'] = $errmsg;
+    $header['content'] = $content;
+    return $header;
+}
+
+$url = "https://www.digikala.com/product/dkp-90825";
+
+$page = get_web_page($url);
